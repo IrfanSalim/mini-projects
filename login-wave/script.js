c37bc6d6544bc318636f0f9a3a5f91ded4c1027d
Irfan <irfansalim77@gmail.com>
2023-09-28 23:53:26 +0530

    Improved mini project 8 to add a new form

--- login-wave/script.js
+++ login-wave/script.js
@@ -1,9 +1,25 @@
-const labels = document.querySelectorAll(".form-group label");
-const register = document.querySelector("a");
+const labels = document.querySelectorAll("#loginForm label");
+const toggleBtns = document.querySelectorAll("a");
+const loginForm = document.querySelector(".login");
+const signupForm = document.querySelector(".register");
 
 labels.forEach((label) => {
     label.innerHTML = label.innerText
         .split("")
         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
         .join("");
+});
+
+toggleBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (loginForm.classList.contains("show")) {
+            // Hide login form and show signup form
+            loginForm.classList.remove("show");
+            signupForm.classList.add("show");
+        } else {
+            // Hide signup form and show login form
+            signupForm.classList.remove("show");
+            loginForm.classList.add("show");
+        }
+    });
 });
